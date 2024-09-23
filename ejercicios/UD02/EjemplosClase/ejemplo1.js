367209ff01bd645a915496b80cb8f4e88b2d98a3
xamorrod <xamorrod537@ieshnosmachado.org>
2024-09-23 12:59:49 +0200

    Código JS primera práctica

---
+++ ejercicios/UD02/EjemplosClase/ejemplo1.js
@@ -0,0 +1,15 @@
+alert("Hola mundo");
+console.log("Escribo en consola desde código");
+let num = 2;
+alert(num);
+num = "Adios mundo";
+alert(num);
+
+//No se recomienda el uso de var en la declaración de variables
+let num2 = 3.15;
+alert(num2);
+
+const PI = Math.PI;
+alert(PI);
+
+
