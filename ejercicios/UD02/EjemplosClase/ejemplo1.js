a7e33675e77854197567708591d93fdf6e2b41e9
xamorrod <xamorrod537@ieshnosmachado.org>
2024-09-26 13:47:11 +0200

    Pequeñas implementaciones de nuevas herramintas

--- ejercicios/UD02/EjemplosClase/ejemplo1.js
+++ ejercicios/UD02/EjemplosClase/ejemplo1.js
@@ -3,13 +3,20 @@ console.log("Escribo en consola desde código");
 let num = 2;
 alert(num);
 num = "Adios mundo";
-alert(num);
+//alert(num);
 
 //No se recomienda el uso de var en la declaración de variables
 let num2 = 3.15;
-alert(num2);
+//alert(num2);
 
 const PI = Math.PI;
-alert(PI);
+//alert(PI);
+
+//El segundo argumento es el valor por defecto
+
+let age = prompt ("¿Cuántos años tienes?" ,100);
+alert ("Tienes " + age + " años");
+
+
 
 
