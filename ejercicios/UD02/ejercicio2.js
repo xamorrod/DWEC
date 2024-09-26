1ecedec80416d89ac3112af6a089df3937a4884a
xamorrod <xamorrod537@ieshnosmachado.org>
2024-09-26 14:02:38 +0200

    Ejercicio 2

---
+++ ejercicios/UD02/ejercicio2.js
@@ -0,0 +1,9 @@
+//Inicializamos las variables en las que pedimos al usuario sus datos
+
+let nombre = prompt("¿Cómo te llamas?", "Por Ejemplo: Jonh");
+let apellido = prompt("¿Cuál es tu apellido?","Por Ejemplo: Doe");
+
+
+//Mostramos la alerta por pantalla con el nombre
+
+alert("Te llamas " + nombre + " " + apellido);
