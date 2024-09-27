7fc8a00edaed03ee6d2ffb9da03cbc1515113445
xamorrod <xamorrod537@ieshnosmachado.org>
2024-09-27 14:07:54 +0200

    Optimización tabla multiplicar

--- ejercicios/UD02/ejercicio3.js
+++ ejercicios/UD02/ejercicio3.js
@@ -1,5 +1,10 @@
 let num = prompt("Introduce un número del que vamos a extraer la tabla de multiplicar");
-
-for(let i = 1 ; i <= 10 ; i++){
-    console.log(num * i)
+let  a = new Array();
+for(let i = 0 ; i <= 10 ; i++){
+    a[i] = i*num;
 }
+console.log(a)
+
+
+
+
