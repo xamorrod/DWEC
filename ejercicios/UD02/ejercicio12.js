6e1922588e9c8ee1328942c962542bcbcd3535ea
xamorrod <xamorrod537@ieshnosmachado.org>
2024-09-30 08:37:22 +0200

    Nuevos ejercicios práctica

---
+++ ejercicios/UD02/ejercicio12.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Introduce un número");
+let num2 = 
+prompt("Introduce otro número");
+
+if( num1 > num2){
+    alert("El resultado de la suma es " + (Number(num1) + Number(num2)) + " y su diferencia es " + (num1 - num2));
+
+}
+else    {
+    alert("El producto es " + num1 * num2 + " y la división es "  + num1 / num2);
+}
---
+++ ejercicios/UD02/ejercicio26.js
@@ -0,0 +1,6 @@
+let sumTot = 0;
+
+for (let i = 0; i < 5; i++) {
+  sumTot += parseInt(prompt("Introduce un valor para sumarlo"));
+}
+alert("El resultado de la suma es " + sumTot);
