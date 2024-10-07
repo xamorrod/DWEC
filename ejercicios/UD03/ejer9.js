018017e877dca38e9a54192321c2a704b8550dd4
xamorrod <xamorrod537@ieshnosmachado.org>
2024-10-07 13:05:11 +0200

    ejercicio 9

---
+++ ejercicios/UD03/ejer9.js
@@ -0,0 +1,29 @@
+function ordenarFecha() {
+  const fecha1 = new Date(document.fechas.fecha1.value);
+  const fecha2 = new Date(document.fechas.fecha2.value);
+  let output = "";
+  console.log(fecha1.getTime());
+  if (fecha1 < fecha2) {
+    output =
+      "La primera fecha es anterior" + distanciaEntreFechas(fecha2, fecha1);
+  } else {
+    output =
+      "La segunda fecha es anterior" + distanciaEntreFechas(fecha1, fecha2);
+  }
+
+  output += document.getElementById("salida").innerHTML = output;
+}
+
+function distanciaEntreFechas(fecha1, fecha2) {
+  let año = fecha1.getYear() - fecha2.getYear();
+  let mes = fecha1.getMonth() - fecha2.getMonth();
+  let dia = fecha1.getDate() - fecha2.getDate();
+  return (
+    "La diferencia de días es " +
+    Math.abs(dia) +
+    " la diferencia de meses es " +
+    Math.abs(mes) +
+    " la diferencia de años es " +
+    Math.abs(año)
+  );
+}
